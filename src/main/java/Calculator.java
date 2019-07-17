536a5483b3a6a964726d293225edf9038c34afe2
AShnyakin <allexxmany@gmail.com>
2019-07-17 23:25:36 +0300

    Add method for max numbers

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,4 +4,16 @@ public class Calculator {
         return a + b;
     }
 
+    public static int getNumberOfMaxParam(int a, int b, int c) {
+        int max1 = Math.max(a,b);
+        int max = Math.max(max1,c);
+        if(max==a) {
+            return 1;
+        } else if (max==b) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
 }
